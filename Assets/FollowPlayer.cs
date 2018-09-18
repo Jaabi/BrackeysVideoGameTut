324d683bbade3ffe6d532ff071322be05af47e3c
Jaabi <abdieliscool3@gmail.com>
2018-09-18 00:24:49 -0500

    Finished video 4: CAMERA FOLLOW

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -4,8 +4,11 @@ public class FollowPlayer : MonoBehaviour {
 
     public Transform player;
 
+    public Vector3 offset;
+
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(player.position);
+        //NOTE: This 'transform' is the Transform of THIS game object! :D
+        transform.position = player.position + offset;
 	}
 }

df7e6aa33df70436877c708b7740e678ed95cc7b
Jaabi <abdieliscool3@gmail.com>
2018-09-19 00:06:23 -0500

    Finished video 7: SCORE & UI

---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    //Variables
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // This is used to mess with physics in Unity whereas the Update method is
+    // used for the once-per-frame way of updating things.
+	void FixedUpdate ()
+    {
+        //Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        //User input
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}

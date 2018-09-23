880492b864c2a09c66fcaa45c9cb97dbe482b61c
Jaabi <abdieliscool3@gmail.com>
2018-09-23 00:54:07 -0500

    Finished last video: FINISHING UP

---
+++ Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour {
+
+    //Load the next level
+	public void LoadNextLevel()
+    {
+        int currLevelIdx = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currLevelIdx + 1);
+    }
+
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    public void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+
+            //Find Game Manager and end the game
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+
+}
